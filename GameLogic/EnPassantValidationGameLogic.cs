c65fc43bd2e3e54e3119188d6507af72873b879d
QGJ <qgj.devel@gmail.com>
2022-10-25 15:38:44 +0200

    simplify CanCaptureEnPassant() method

--- GameLogic/EnPassantValidationGameLogic.cs
+++ GameLogic/EnPassantValidationGameLogic.cs
@@ -14,17 +14,14 @@ namespace ChessDotNET.GameLogic
             ChessPieceColor oldCoordsColor = tileDict[oldCoords.String].ChessPiece.ChessPieceColor;
             ChessPieceColor pawnMovedTwoTilesColor = tileDict[tileDict.CoordsPawnMovedTwoTiles.String].ChessPiece.ChessPieceColor;
 
-            if (tileDict[oldCoords.String].ChessPiece.ChessPieceType == ChessPieceType.Pawn && oldCoordsColor != pawnMovedTwoTilesColor)
+            if (tileDict[oldCoords.String].ChessPiece.ChessPieceType == ChessPieceType.Pawn
+                && oldCoordsColor != pawnMovedTwoTilesColor
+                && tileDict[oldCoords.String].Y == tileDict.CoordsPawnMovedTwoTiles.Y
+                && tileDict.CoordsPawnMovedTwoTiles.X == newCoords.X
+                && (oldCoordsColor == ChessPieceColor.White && tileDict[newCoords.String].Y - tileDict.CoordsPawnMovedTwoTiles.Y == 1
+                || oldCoordsColor == ChessPieceColor.Black && tileDict[newCoords.String].Y - tileDict.CoordsPawnMovedTwoTiles.Y == -1))
             {
-                if (tileDict[oldCoords.String].Y == tileDict.CoordsPawnMovedTwoTiles.Y
-                    && (oldCoordsColor == ChessPieceColor.White && tileDict[newCoords.String].Y - tileDict.CoordsPawnMovedTwoTiles.Y == 1
-                    || oldCoordsColor == ChessPieceColor.Black && tileDict[newCoords.String].Y - tileDict.CoordsPawnMovedTwoTiles.Y == -1))
-                {
-                    if (tileDict.CoordsPawnMovedTwoTiles.X == newCoords.X)
-                    {
-                        return true;
-                    }
-                }
+                return true;
             }
 
             return false;
